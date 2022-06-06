191595094cdd6a891050c608556ef65ea9657747
Hovhannes Hovakimyan <Hovhannes.Hovakimyan@cantire.com>
2022-06-06 17:53:52 +0400

    initial commit, main RG creation

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group"
+  default     = "demotfrg"
+}
+
+variable "rg_location" {
+  type        = string
+  description = "Location for the resource group"
+  default     = "West Europe"
+}
